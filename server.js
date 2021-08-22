d60b2be92e623bd4ec65ac0a82674bc2dc8123d1
Kiera <kiera.chelsea3@gmail.com>
2021-08-22 14:14:04 +0100

    code cleanup

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require("express");
-const app = express();
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
-//controllers imported:
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
-
+/* const PORT = process.env.PORT;
+ */
 const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl: true,
   },
 });
 
+const app = express();
 
 //FIXED CORRS ERROR WITH npm install cors -> used here
 app.use(cors());
@@ -27,17 +25,51 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//TEST
+app.get("/", (req, res) => {
+  res.send('it;s working!!');
+});
 
+//SIGN IN AN EXISTING USER
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+//REGISTERING A NEW USER
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+//FOR FUTURE APP EXPANSION
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+//COUNTING THE NR OF TIMES YOU USED THE IMAGE DETECTOR
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+});
+
+
+
+
+
+
+/* 
 //TEST
 app.get("/", (req, res) => {
   res.send("it is working!!");
 });
 
-//SIGN IN AN EXISTING USER
-/* app.post("/signin", (req, res) => {
-  signin.handleSignin(req, res, db, bcrypt);
-});
- */
+
 app.post("/signin", signin.handleSignin(db, bcrypt));
 
 //REGISTERING A NEW USER
@@ -59,3 +91,5 @@ app.post("/imageurl", (req, res) => {
 //SET LOCAL PORT:
 app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`)});
+
+  */

5a54ac35010b72e693cb7daa79f2a2ae07ee9177
Kiera <kiera.chelsea3@gmail.com>
2021-08-22 13:37:57 +0100

    code cleanup

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const app = express();
 const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
+//controllers imported:
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
-/* const PORT = process.env.PORT;
- */
+//DATABASE LINKED:
 const db = knex({
   client: "pg",
   connection: {
@@ -19,13 +20,13 @@ const db = knex({
   },
 });
 
-const app = express();
 
 //FIXED CORRS ERROR WITH npm install cors -> used here
+app.use(express.json());
 app.use(cors());
 //REPLACED BODYPARSER WITH THIS:
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+
 
 //TEST
 app.get("/", (req, res) => {
@@ -33,29 +34,28 @@ app.get("/", (req, res) => {
 });
 
 //SIGN IN AN EXISTING USER
-app.post("/signin", (req, res) => {
+/* app.post("/signin", (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
 });
+ */
+app.post("/signin", signin.handleSignin(db, bcrypt));
 
 //REGISTERING A NEW USER
 app.post("/register", (req, res) => {
-  register.handleRegister(req, res, db, bcrypt);
+  register.handleRegister(req, res, db, bcrypt)
 });
 
 //FOR FUTURE APP EXPANSION
 app.get("/profile/:id", (req, res) => {
-  profile.handleProfileGet(req, res, db);
-});
+  profile.handleProfileGet(req, res, db)})
 
 //COUNTING THE NR OF TIMES YOU USED THE IMAGE DETECTOR
 app.put("/image", (req, res) => {
-  image.handleImage(req, res, db);
-});
+  image.handleImage(req, res, db)});
 
 app.post("/imageurl", (req, res) => {
-  image.handleApiCall(req, res);
-});
+  image.handleApiCall(req, res)});
 
+//SET LOCAL PORT:
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT}`);
-});
+  console.log(`app is running on port ${process.env.PORT}`)});
